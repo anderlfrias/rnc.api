551b3f779e5c68e22aa49a11bff01f7c8b113bc5
Anderson Frias <anderl.friaas@gmail.com>
2024-06-28 15:24:51 -0400

    [Update] Liberacion del endpoint de obtener rnc

--- config/policies.js
+++ config/policies.js
@@ -18,7 +18,7 @@ module.exports.policies = {
   ***************************************************************************/
 
   '*': true,
-  RNCController: {
-    '*': 'isLoggedIn',
-  },
+  // RNCController: {
+  //   '*': 'isLoggedIn',
+  // },
 };
